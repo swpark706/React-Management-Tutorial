896b7be231f5c952e966cd8997315836d99c2ce2
sewoongpark <sewoong2890@gmail.com>
2020-01-13 15:05:11 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -4,23 +4,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develope a managament system</h2>
     </div>
   );
 }
 
 export default App;
+
+//실질적인 웹사이트의 내용
